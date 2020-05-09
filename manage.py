41b3623f1f15108f4b8c5194018306f06e2ea3be
RiddhiGupta5 <mail2riddhigupta@gmail.com>
2020-05-10 01:13:35 +0530

    Renamed App

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackathon.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
